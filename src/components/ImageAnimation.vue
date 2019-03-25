0deb2a84bf5dbcf728d740bd84421e9fba2d0cb4
Peter Joles <jolzee@gmail.com>
2019-03-25 16:43:38 -0700

    slight hack - trying to get some images to display small

--- src/components/ImageAnimation.vue
+++ src/components/ImageAnimation.vue
@@ -7,8 +7,8 @@
   <v-img
     v-if="url"
     :src="url"
+    :max-height="determineMaxHeight()"
     contain
-    height="auto"
   ></v-img>
   <!-- </transition> -->
 </template>
@@ -17,6 +17,14 @@ export default {
   props: ["url"],
   data() {
     return {};
+  },
+  methods: {
+    determineMaxHeight() {
+      if (this.url.includes("/weather/")) {
+        return 100;
+      }
+      return "230";
+    }
   }
 };
 </script>
