2cb351fd6b9293a8a85c29ca7c9de94ea4f2ee0d
Peter Joles <jolzee@gmail.com>
2019-09-24 11:52:50 -0700

    [plug]

--- src/main.js
+++ src/main.js
@@ -15,8 +15,12 @@ import { getStore } from "./store";
 import App from "./App";
 import router from "./router";
 
-console.groupCollapsed("Powered by Leopard 💬");
-console.log("Leopard Chat UI");
+console.groupCollapsed(
+  `%c Powered by %c Leopard Chat UI 💬 %c`,
+  "background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff",
+  "background:#41b883 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff",
+  "background:transparent"
+);
 console.log("Author: Peter Joles - peter.joles@artificial-solutions.com");
 console.log("Documentation: https://jolzee.gitbook.io/leopard/");
 console.log("Code: https://github.com/jolzee/chat-teneo-vue");
