b1ffaf1a04937fe6ad2c4fbe8b19b5709c47811a
Peter Joles <jolzee@gmail.com>
2019-11-04 16:20:20 -0800

    Cleaned up look and feel of custom card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,35 +3,54 @@
     class="mx-auto mt-2"
     max-width="400"
   >
-    <v-img v-if="config.imageUrl"
+    <v-img
+      v-if="config.imageUrl"
       class="white--text align-end"
-      height="200px"
+      contain
       :src="config.imageUrl"
     >
-      <v-card-title class="cardTitleBackground" v-if="config.title">{{ config.title }}</v-card-title>
     </v-img>
-    <v-card-title v-else-if="config.title" class="mb-2">{{ config.title }}</v-card-title>
+    <v-card-title
+      class="subtitle-2 white--text primary cardTitleBackground mb-5"
+      v-if="config.title"
+    >{{ config.title }}</v-card-title>
+    <v-card-subtitle
+      v-if="config.subTitle"
+      class="pb-1"
+    >{{ config.subTitle }}</v-card-subtitle>
 
-    <v-card-subtitle v-if="config.subTitle" class="pb-1">{{ config.subTitle }}</v-card-subtitle>
-
-    <v-card-text v-if="config.bodyText" class="text--primary">
+    <v-card-text
+      v-if="config.bodyText"
+      class="text--primary"
+    >
       {{ config.bodyText }}
     </v-card-text>
 
-    <v-card-text v-if="config.chips" class="my-0 py-0">
+    <v-card-text
+      v-if="config.chips"
+      class="my-0 py-0"
+    >
       <v-chip-group
         v-model="chipSelectionIndex"
         active-class="secondary white--text"
         column
       >
-        <v-chip v-for="(chip, index) in config.chips" :key="'chip' + index">{{ chip.name }}</v-chip>
+        <v-chip
+          v-for="(chip, index) in config.chips"
+          :key="'chip' + index"
+        >{{ chip.name }}</v-chip>
       </v-chip-group>
     </v-card-text>
 
     <v-card-actions v-if="config.actions">
       <v-spacer></v-spacer>
-      <v-btn :disabled="config.chips && chipSelectionIndex === null" v-for="(action, index) in config.actions" :key="'action' + index"
-        small color="primary" @click="actionClicked(action)"
+      <v-btn
+        :disabled="config.chips && chipSelectionIndex === null"
+        v-for="(action, index) in config.actions"
+        :key="'action' + index"
+        small
+        color="primary"
+        @click="actionClicked(action)"
       >
         {{ action.name }}
       </v-btn>
@@ -44,13 +63,15 @@ export default {
   props: ["item"],
   data() {
     return {
-      chipSelectionIndex : null
+      chipSelectionIndex: null
     };
   },
   computed: {
     config() {
-      let theConfig = decodeURIComponent(this.item.teneoResponse.extraData.displayCard);
-      return JSON.parse(theConfig)
+      let theConfig = decodeURIComponent(
+        this.item.teneoResponse.extraData.displayCard
+      );
+      return JSON.parse(theConfig);
     }
   },
   methods: {
@@ -58,11 +79,11 @@ export default {
       let responseText = "";
       let responseParameters = "";
       if (this.chipSelectionIndex !== null) {
-          let selectedChip = this.config.chips[this.chipSelectionIndex];
-          responseText = selectedChip.name;
-          responseParameters = "&" + selectedChip.params;
+        let selectedChip = this.config.chips[this.chipSelectionIndex];
+        responseText = selectedChip.name;
+        responseParameters = "&" + selectedChip.params;
       } else {
-          responseText = action.name;
+        responseText = action.name;
       }
       responseParameters += "&" + action.params;
       this.$store.commit("SHOW_PROGRESS_BAR");
@@ -71,7 +92,7 @@ export default {
       this.$store
         .dispatch(
           "sendUserInput",
-          responseParameters !== ''
+          responseParameters !== ""
             ? responseParameters + optionClickParam
             : optionClickParam
         )
@@ -79,13 +100,9 @@ export default {
           console.log("Card info sent to Teneo");
         });
     }
-  },
+  }
 };
 </script>
 
 <style>
-.cardTitleBackground {
-  background-color: rgba(17,18,25,0.5);
-}
-
 </style>
