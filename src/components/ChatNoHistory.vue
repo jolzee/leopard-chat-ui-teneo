5005bccbb6ecc68339e7e2d0c577190dd33452eb
Peter Joles <jolzee@gmail.com>
2019-12-03 16:52:43 -0800

    Goodbye old no chat history image

--- src/components/ChatNoHistory.vue
+++ src/components/ChatNoHistory.vue
@@ -2,28 +2,31 @@
   <v-row no-gutters>
     <v-col cols="12">
       <v-card>
-        <v-img :src="noHistoryImage" class="elevation-2"></v-img>
         <v-card-title primary-title>
           <v-card-text>
             <div class="text-center">
-              <h3 class="headline mb-2 text-center">
-                {{ $t("no.chat.history.title") }}
-              </h3>
+              <h3 class="headline mb-2 text-center">{{ $t("no.chat.history.title") }}</h3>
               <div class="text-center">{{ $t("no.chat.history.body") }}</div>
             </div>
           </v-card-text>
         </v-card-title>
         <v-row justify="center" class="pb-3">
           <v-card-actions>
-            <v-btn dark color="primary" :to="{ name: 'chat' }">{{
+            <v-btn dark color="primary" :to="{ name: 'chat' }">
+              {{
               $t("menu.chat")
-            }}</v-btn>
-            <v-btn dark color="primary" :to="{ name: 'help' }">{{
+              }}
+            </v-btn>
+            <v-btn dark color="primary" :to="{ name: 'help' }">
+              {{
               $t("menu.help")
-            }}</v-btn>
-            <v-btn dark color="primary" :to="{ name: 'about' }">{{
+              }}
+            </v-btn>
+            <v-btn dark color="primary" :to="{ name: 'about' }">
+              {{
               $t("menu.about")
-            }}</v-btn>
+              }}
+            </v-btn>
           </v-card-actions>
         </v-row>
       </v-card>
@@ -34,10 +37,8 @@
 <script>
 const logger = require("@/utils/logging").getLogger("ChatNoHistory.vue");
 export default {
-  computed: {
-    noHistoryImage() {
-      return require("../assets/history.png");
-    }
+  data() {
+    return {};
   }
 };
 </script>
