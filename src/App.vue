423dd5d00e9f375e4251a18c84f027191daf2f08
Peter Joles <jolzee@gmail.com>
2019-12-09 20:07:55 -0800

    Add ease animation to window when resized less than 480px and back

--- src/App.vue
+++ src/App.vue
@@ -1143,6 +1143,7 @@ a:focus {
 }
 
 .v-application {
+  transition: 0.3s ease-in-out;
   max-width: 360px;
   max-height: 100vh;
   height: 100vh !important;
@@ -1241,6 +1242,7 @@ iframe#site-frame {
   }
 
   .v-application {
+    transition: 0.3s ease-in-out;
     max-width: 100% !important;
     max-height: 100% !important;
     height: 100% !important;
