44e99822404f2d5b3b85d73cc1d4f24b2100480e
Peter Joles <jolzee@gmail.com>
2020-05-27 14:12:25 -0700

    Allow ?width= to be read from image urls and then used to size image

--- src/components/ImageAnimation.vue
+++ src/components/ImageAnimation.vue
@@ -20,6 +20,20 @@
   <!-- </transition> -->
 </template>
 <script>
+
+var getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
 export default {
   props: {
     url: {
@@ -32,10 +46,12 @@ export default {
   },
   methods: {
     determineMaxHeight() {
-      if (this.url.includes("/weather/")) {
-        return 100;
+      let params = getParams(this.url);
+      if (params.width) {
+        return params.width;
+      } else {
+        return "230";
       }
-      return "230";
     }
   }
 };
