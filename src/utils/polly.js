32b88d1dbdb30d220ccd1c492f7233026df18672
Peter Joles <jolzee@gmail.com>
2020-06-22 15:57:10 -0700

    fix: Polly problems now with promise

--- src/utils/polly.js
+++ src/utils/polly.js
@@ -3,9 +3,9 @@ export default class Polly {
     this.audio = null;
   }
 
-  say(text, voice) {
+  async say(text, voice) {
     if (text && voice && this.audio) {
-      this.waitForPollyToFinish();
+      await this.waitForPollyToFinish();
       this.playAudio(text, voice);
     } else if (text && voice) {
       this.playAudio(text, voice);
@@ -20,13 +20,18 @@ export default class Polly {
     this.audio.play();
   }
 
-  async waitForPollyToFinish() {
-    while (true) {
+  waitForPollyToFinish() {
+    return new Promise(resolve => {
       if (this.isPaused()) {
-        return;
+        resolve();
       }
-      await null; // prevents app from hanging
-    }
+      let that = this;
+      setInterval(() => {
+        if (that.isPaused()) {
+          resolve();
+        }
+      }, 300);
+    });
   }
 
   isPaused() {
