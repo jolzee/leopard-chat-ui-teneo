37439718493ee7eb7fcd81b829d1f3da73f5320c
Peter Joles <jolzee@gmail.com>
2020-06-22 15:16:12 -0700

    feat: Have Polly wait for herself to finish what she's saying when language auto switching

--- src/utils/polly.js
+++ src/utils/polly.js
@@ -3,14 +3,28 @@ export default class Polly {
     this.audio = null;
   }
 
-  say(text, voice) {
+  async say(text, voice) {
     if (text && voice) {
+      await waitForPollyToFinish();
       this.stop();
       this.audio = new Audio(
         `${window.leopardConfig.tts.url}?text=${encodeURIComponent(text)}&voice=${voice}`
       );
       this.audio.play();
     }
+  }
+
+  async waitForPollyToFinish() {
+    while (true) {
+      if (!isPlaying()) {
+        return;
+      }
+      await null; // prevents app from hanging
+    }
+  }
+
+  isPlaying() {
+    return !this.audio.paused;
   }
 
   stop() {
